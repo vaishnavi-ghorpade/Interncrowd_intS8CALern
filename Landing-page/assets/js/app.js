bbcbeb2c720abb2ebfd3272944800a1cc34399af
Vaishnavi <vaishnavighorpade810@gmail.com>
2023-02-19 20:50:56 +0530

    Add files via upload

---
+++ Landing-page/assets/js/app.js
@@ -0,0 +1,48 @@
+const navbar = document.querySelector('.navbar')
+const hamburger = document.querySelector('.hamburger')
+const navLinks = document.querySelector('.nav-links')
+const navLinksLi = document.querySelectorAll('.nav-links li')
+
+
+window.addEventListener('scroll', () => {
+    if(this.scrollY >= 100) {
+        navbar.classList.add('scrolled')
+    } else {
+        navbar.classList.remove('scrolled')
+    }
+})
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active')
+    hamburger.classList.toggle('active')
+})
+
+navLinksLi.forEach(li => li.addEventListener('click', ()=> {
+    navLinksLi.forEach(li => li.classList.remove('active'))
+    li.classList.add('active')
+
+    hamburger.classList.remove('active')
+    navLinks.classList.remove('active')
+
+}))
+
+
+// Typed js
+var options = {
+    strings: [
+        'Web Development', 
+        'Java Development',
+        'App development',
+        'Python Programing'
+    ],
+    typeSpeed: 40,
+    loop: true,
+    loopCount: Infinity,
+    typeSpeed: 10,
+    backDelay: 2000,
+  };
+  
+var typed = new Typed('#hero-titles', options);
+
+// AOS
+AOS.init();
